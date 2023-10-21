b7cbb1e6fdff778f47bf928a14a49b84dbdbe54e
bhuneshk <kumarbhunesh3@gmail.com>
2023-10-21 12:19:25 +0530

    authtoken generated

--- backend/modules/User.js
+++ backend/modules/User.js
@@ -8,6 +8,7 @@ const UserSchema = new Schema({
     email:{
         type: String,
         required: true,
+        unique: true
     },
     password:{
         type: String,
@@ -18,5 +19,6 @@ const UserSchema = new Schema({
         default: Date.now
     }
 })
-
-module.exports = mongoose.model('user',UserSchema);
+const User=mongoose.model('user',UserSchema);
+User.createIndexes;
+module.exports = User;
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,16 +1,40 @@
-const express= require('express');
+const express = require('express');
 const router = express.Router();
-const User= require('../modules/User');
-const {body,validationResult} = require('express-validator');
-router.post('/',[body('name').isLength({min:3}),body('email').isEmail(),body('password').isLength({min:3})],(req,res)=>{
-    const result = validationResult(req);
-     if (result.isEmpty()) {
-    const user=User(req.body);
-    user.save();
-    return res.send(req.body);
-  }
+const User = require('../modules/User');
+const { body, validationResult } = require('express-validator');
+const bcrypt=require('bcryptjs');
+const jwt=require('jsonwebtoken')
+const JWT_SECRET='Bhuneshisagood$oy';
 
-  res.send({ errors: result.array() });
+router.post('/', [body('name', 'Enter Valid Username').isLength({ min: 3 }), body('email', 'Enter Valid Email').isEmail(), body('password', 'Password should be minimum 5 char').isLength({ min: 5 })], async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+    try{
+        let user=await User.findOne({email: req.body.email });
+        if(user){
+            return res.status(400).json({error: "Sorry user with this email already exists"});
+        }
+        const salt= await bcrypt.genSalt(10);
+        const secPass= await bcrypt.hash(req.body.password,salt);
+        user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: secPass
+        })
+        const data={
+            user:{
+                id: user.id
+            }
+        }
+        const authtoken= jwt.sign(data,JWT_SECRET);
+
+        res.json({authtoken});
+    }catch(error){
+        console.log(error);
+        res.status(500).send("Some error occured")
+    }
 })
 
-module.exports=router;
+module.exports = router;
