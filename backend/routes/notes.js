76ceae1ec24931e4b4555013af93dd2475387c06
bhuneshk <kumarbhunesh3@gmail.com>
2023-10-22 18:19:28 +0530

    backend is setup

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -4,13 +4,20 @@ const Notes= require('../modules/Notes')
 const {body, validationResult}= require('express-validator');
 const fetchuser = require('../middleware/fetchuser');
 router.get('/fetchallnotes',fetchuser, async (req,res)=>{
+    try {
+        
     const notes = await Notes.find({user: req.user.id})
     res.json(notes);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Internal Server Error');
+    }
 })
 
 router.get('/addnote',fetchuser,[body('title').isLength({min:3}),body('description').isLength({min: 5})], async (req,res)=>{
     const {title, description, tag} = req.body;
-
+    try {
+        
     const errors = validationResult(req);
     if(!errors.isEmpty()){
         return res.status(400).json({errors: errors.array()});
@@ -22,10 +29,15 @@ router.get('/addnote',fetchuser,[body('title').isLength({min:3}),body('descripti
 
     const savedNote = await notes.save();
     res.send(savedNote);
+} catch (error) {
+    console.error(error.message);
+    res.status(500).send('Internal Server Error');
+}
 })
 
 router.put('/updatenote/:id',fetchuser, async (req,res)=>{
-    
+    try {
+        
     let note = await Notes.findById(req.params.id);
     if(!note){
         return res.status(404).send("Not Found");
@@ -48,10 +60,15 @@ router.put('/updatenote/:id',fetchuser, async (req,res)=>{
     
     note = await Notes.findByIdAndUpdate(req.params.id,{$set : newNote},{new:true})
     res.send(note);
+} catch (error) {
+    console.error(error.message);
+    res.status(500).send('Internal Server Error');
+}
 })
 
 router.delete('/deletenote/:id',fetchuser, async (req,res)=>{
-    
+    try {
+        
     let note = await Notes.findById(req.params.id);
     if(!note){
         return res.status(404).send("Not Found");
@@ -63,6 +80,10 @@ router.delete('/deletenote/:id',fetchuser, async (req,res)=>{
     
     await Notes.findByIdAndDelete(req.params.id)
     res.send('Deleted');
+} catch (error) {
+    console.error(error.message);
+    res.status(500).send('Internal Server Error');
+}
 })
 
 module.exports=router;
